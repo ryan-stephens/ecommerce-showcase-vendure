94a7a614f76f109713d4dbddc81f9ef1aa8734ad
Ryan Stephens <ryan.stephens15@gmail.com>
2025-08-02 22:47:08 -0500

    attempt to fix test data images

--- src/vendure-config.ts
+++ src/vendure-config.ts
@@ -63,7 +63,7 @@ export const config: VendureConfig = {
         GraphiqlPlugin.init(),
         AssetServerPlugin.init({
             route: 'assets',
-            assetUploadDir: path.join(__dirname, '../static/assets'),
+            assetUploadDir: path.join(process.cwd(), 'static', 'assets'),
             // For local dev, the correct value for assetUrlPrefix should
             // be guessed correctly, but for production it will usually need
             // to be set manually to match your production url.
